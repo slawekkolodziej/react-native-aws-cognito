9834030e27b149d918603ea6627feb3dad44f9a1
Slawek Kolodziej <hfrntt@gmail.com>
2017-04-14 12:06:54 +0200

    Reimplement index.js as simple CommonJS module

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import { NativeModules } from 'react-native';
-export default NativeModules.RNAWSCognito;
+var NativeModules = require('react-native').NativeModules;
+module.exports = NativeModules.RNAWSCognito;
